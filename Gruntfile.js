43d97fb20f9af1e55ab3b0372883ed1530e00e99
Miguel Castillo <manchagnu@gmail.com>
2015-01-25 10:07:52 -0500

    Added grunt tasks for livereload and moved the build from npm to `grunt build`

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,16 +7,16 @@ module.exports = function(grunt) {
     connect: {
       test: {
         options: {
-          port: 8612,
+          port: 8012,
           hostname: 'localhost'
         }
       },
       keepalive: {
         options: {
-          port: 8619,
+          port: 8010,
           host: "localhost",
           keepalive: true,
-          open: "http://localhost:8619/test/SpecRunner.html"
+          open: "http://localhost:8010/test/SpecRunner.html"
         }
       }
     },
@@ -28,14 +28,60 @@ module.exports = function(grunt) {
           reporter: "Spec",
           run: false,
           timeout: 10000,
-          urls: ["http://localhost:8612/test/SpecRunner.html"]
+          urls: ["http://localhost:8012/test/SpecRunner.html"]
+        }
+      }
+    },
+    watch: {
+      test: {
+        files: ['src/**/*.js', 'test/**/*.js', '*.js'],
+        tasks: ['jshint:all', 'build'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    jshint: {
+      all: {
+        options: {
+          reporter: require('jshint-stylish')
+        },
+        src: ['src/**.*.js', 'test/**/*.js', '*.js']
+      }
+    },
+    concurrent: {
+      test: {
+        tasks: ['connect:keepalive', 'watch:test'],
+        options: {
+          logConcurrentOutput : true
+        }
+      }
+    },
+    browserify: {
+      "build-debug": {
+        files: {
+          "dist/bit-imports.js": ["src/bit-imports.js"]
+        },
+        options: {
+          browserifyOptions: {
+            "detectGlobals": true,
+            "ignoreMissing": true,
+            "standalone": "bit-imports"
+          }
         }
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-mocha");
+  grunt.loadNpmTasks("grunt-concurrent");
   grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-browserify");
+
+  grunt.registerTask("build", ["browserify:build-debug"]);
   grunt.registerTask("server", ["connect:keepalive"]);
   grunt.registerTask("test", ["connect:test", "mocha:test"]);
+  grunt.registerTask("livereload", ["concurrent:test"]);
 };
