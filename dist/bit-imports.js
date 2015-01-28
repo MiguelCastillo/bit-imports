a0802d0acfd2acdd619d0a18ea5774db9145b918
Miguel Castillo <manchagnu@gmail.com>
2015-01-27 22:09:09 -0500

    Updated pulling-deps

--- dist/bit-imports.js
+++ dist/bit-imports.js
@@ -7592,28 +7592,50 @@ function isNullOrUndefined(arg) {
   var acorn = require('acorn'),
       walk  = require('acorn/util/walk');
 
-  function parse(source, options) {
-    var result = {dependencies: []};
-    var ast = acorn.parse(source, options);
+  /**
+   * Method to pull dependencies from a JavaScript source string.
+   *
+   * @param {string} source - Source to parse
+   * @param {object} options - Options passed to acorn
+   *
+   * @returns {object:{array: dependencies}} - Object with dependencies
+   */
+  function PullDeps(source, options) {
+    return PullDeps.walk(acorn.parse(source, options));
+  }
 
-    walk.simple(ast, {
-      'CallExpression': function callExpression(node) {
-        if (isName(node.callee, TokenTypes._require)) {
-          if (isDependencyString(node.arguments)) {
-            result.dependencies.push(node.arguments[0].value);
-          }
-        }
-        else if (isName(node.callee, TokenTypes._define)) {
-          var dependencies = getDependencyArray(node.arguments);
-          if (dependencies && dependencies.length) {
-            result.dependencies = result.dependencies.concat(dependencies);
-          }
+
+  /**
+   * Method to pull dependencies from an AST.
+   *
+   * @param {object} ast - AST to traverse in order to find all dependencies.
+   *
+   * @returns {object:{array: dependencies}} - Object with dependencies
+   */
+  PullDeps.walk = function(ast) {
+    var result = {dependencies: []};
+
+    function callExpression(node) {
+      if (isName(node.callee, TokenTypes._require)) {
+        var dependency = getDependencyString(node.arguments);
+        if (dependency) {
+          result.dependencies.push(dependency);
         }
       }
+      else if (isName(node.callee, TokenTypes._define)) {
+        var dependencies = getDependencyArray(node.arguments);
+        if (dependencies && dependencies.length) {
+          result.dependencies = result.dependencies.concat(dependencies);
+        }
+      }
+    }
+
+    walk.simple(ast, {
+      'CallExpression': callExpression
     });
 
     return result;
-  }
+  };
 
 
   function isName(node, name) {
@@ -7621,17 +7643,22 @@ function isNullOrUndefined(arg) {
   }
 
 
-  function isDependencyString(node) {
-    return node.length === 1 && TokenTypes.Literal === node[0].type;
+  function getDependencyString(nodes) {
+    if (nodes.length === 1 && TokenTypes.Literal === nodes[0].type) {
+      return nodes[0].value;
+    }
   }
 
 
   function getDependencyArray(nodes) {
     var elements, i, length;
-    if (nodes[0] && nodes[0].type === TokenTypes.ArrayExpression) {
+
+    // Handle define([], function() {}) format
+    if (isArrayExpession(nodes[0])) {
       elements = nodes[0].elements;
     }
-    else if (nodes[1] && nodes[1].type === TokenTypes.ArrayExpression) {
+    // Handle define("modulename", [], function() {}) format
+    else if (isArrayExpession(nodes[1])) {
       elements = nodes[1].elements;
     }
 
@@ -7645,7 +7672,12 @@ function isNullOrUndefined(arg) {
   }
 
 
-  module.exports = parse;
+  function isArrayExpession(node) {
+    return node && TokenTypes.ArrayExpression === node.type;
+  }
+
+
+  module.exports = PullDeps;
 })();
 
 },{"acorn":1,"acorn/util/walk":2}],13:[function(require,module,exports){
