5a1281c5e34b796342b536a8b89e735a4b601f3d
Miguel Castillo <manchagnu@gmail.com>
2015-06-03 20:45:11 -0400

    tweaked coloring and text

--- example/view/Highlights.js
+++ example/view/Highlights.js
@@ -8,16 +8,19 @@ class Highlights extends Component {
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <h3 class="text-center"><i class="fa fa-files-o"></i> Formats</h3>
-            <div>Building applications generally means dependencies of incompatible module formats. For this, bit imports supports AMD and CJS out of the box.</div>
+            <h1 class="text-center"><i class="fa fa-files-o"></i></h1>
+            <h3 class="text-center">Formats</h3>
+            <div class="text-muted">Building applications generally means dependencies of incompatible module formats. For this, bit imports supports AMD and CJS out of the box.</div>
           </div>
           <div class="col-md-4">
-            <h3 class="text-center"><i class="fa fa-plug"></i> Pluggable</h3>
-            <div>Simple and flexible plugin system for building pipelines to process your JavaScript, and even add support for other file types.</div>
+            <h1 class="text-center"><i class="fa fa-plug"></i></h1>
+            <h3 class="text-center">Pluggable</h3>
+            <div class="text-muted">Simple and flexible plugin system for building pipelines to process your assets.</div>
           </div>
           <div class="col-md-4">
-            <h3 class="text-center"><i class="fa fa-list-alt"></i> Browser</h3>
-            <div>Remove your bundling step when developing your Application. Only bundle when you really need to - before deploying to production.</div>
+            <h1 class="text-center"><i class="fa fa-list-alt"></i></h1>
+            <h3 class="text-center">Browser</h3>
+            <div class="text-muted">Remove your bundling step when developing your Application. Only bundle when you really need to - before deploying to production.</div>
           </div>
         </div>
       </div>
