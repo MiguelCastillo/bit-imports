62499c82c4f21dcd948406f7de5b0c0c54ddac83
Miguel Castillo <manchagnu@gmail.com>
2015-06-27 23:09:18 -0400

    added build date to the banner in the build artifacts. better when developing

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function(grunt) {
         src: ["browser.js"],
         dest: "dist/<%= pkg.name %>.js",
         options: {
-          banner: "/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today('yyyy-mm-dd') %>. (c) <%= grunt.template.today('yyyy') %> Miguel Castillo. Licensed under MIT */",
+          banner: "/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today() %>. (c) <%= grunt.template.today('yyyy') %> Miguel Castillo. Licensed under MIT */",
           browserifyOptions: {
             "detectGlobals": true,
             "ignoreMissing": true,
--- example/config.js
+++ example/config.js
@@ -9,14 +9,14 @@ var System = (function() {
     "paths": {
       "babel": "../node_modules/babel-bits/dist/index.min.js",
       "sass": "../node_modules/sassy-bits/dist/index.min.js",
+      "plugin1": "./js/plugin1.js"
     }
   });
-
 
   // Setup js pipeline
   importer.plugin("js", {
     match: {
-      path: extension('js')
+      path: extension("js")
     },
     transform: {
       handler: "babel",
@@ -26,19 +26,18 @@ var System = (function() {
     }
   });
 
-
   // Setup sass pipeline
   importer.plugin("sass", {
     match: {
-      path: extension('css|scss')
+      path: extension("css|scss")
     },
     transform: "sass"
   });
 
-
   return importer;
 })();
 
+// Expose `require` globally
 var require = System.require;
 
 // Load application
