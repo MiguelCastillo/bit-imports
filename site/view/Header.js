53f96a7cf10af9e5689a1ad9d55ec34903608dd2
Miguel Castillo <manchagnu@gmail.com>
2016-02-07 13:05:51 -0500

    Fixed paths for images loaded by the header

--- site/view/Header.js
+++ site/view/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
               </a>
             </li>
             <li>
-              <a href="/docs">
+              <a href="/docs/">
                 Docs
               </a>
             </li>
@@ -21,24 +21,24 @@ class Header extends Component {
         <ul class="nav-middle">
             <li>
               <a href="home">
-                <img id="bit-imports-mini" src="img/bit-imports.svg" alt="bit imports" class="mini-logo">
+                <img id="bit-imports-mini" src="/img/bit-imports.svg" alt="bit imports" class="mini-logo">
               </a>
             </li>
         </ul>
         <ul class="nav-right">
             <li>
               <a href="https://twitter.com/bitsjs" target="_blank">
-                <img src="./img/twitter.svg" alt="Twitter @bitsjs">
+                <img src="/img/twitter.svg" alt="Twitter @bitsjs">
               </a>
             </li>
             <li>
               <a href="https://github.com/MiguelCastillo/bit-imports" target="_blank">
-                <img src="./img/github.svg" alt="GitHub bit imports">
+                <img src="/img/github.svg" alt="GitHub bit imports">
               </a>
             </li>
             <li>
               <a href="https://gitter.im/MiguelCastillo/bit-imports" target="_blank">
-                <img src="./img/gitter.svg" alt="Gitter bit imports">
+                <img src="/img/gitter.svg" alt="Gitter bit imports">
               </a>
             </li>
           </ul>
