4f1d135bf99c45b36646a3616c9e6d5ee1a6887b
Miguel Castillo <manchagnu@gmail.com>
2016-02-10 12:14:47 -0500

    Built bit-imports from commit 5bd40c9 on branch master

--- view/Docs.js
+++ view/Docs.js
@@ -3,7 +3,7 @@ import Component from "../js/Component";
 class Docs extends Component {
   render() {
     return this.content `
-      <iframe src="docs/global.html" style="flex: 1; border:0px;"></iframe>
+      <iframe src="_docs/global.html" style="flex: 1; border:0px;"></iframe>
     `;
   }
 }
