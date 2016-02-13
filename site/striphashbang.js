63e09bd77000c9a33e545ef69eb7f003273d8e91
Miguel Castillo <manchagnu@gmail.com>
2016-02-12 23:45:50 -0500

    Added check hashset to test if the incoming URL has a hash bang

--- site/striphashbang.js
+++ site/striphashbang.js
@@ -1,12 +1,14 @@
 (function() {
-  var rewrite = window.location.href.replace(/#!?/, "");
-  if (typeof(window.history.replaceState) == 'function') {
-    window.history.replaceState(null, "Initial entry", rewrite);
-  }
-  else if (typeof(window.history.pushState) == 'function') {
-    window.history.pushState(null, "Initial entry", rewrite);
-  }
-  else {
-    window.location.hash = rewrite;
+  if (/#!?/.test(window.location.href)) {
+    var rewrite = window.location.href.replace(/#!?/, "");
+    if (typeof(window.history.replaceState) == 'function') {
+      window.history.replaceState(null, "Initial entry", rewrite);
+    }
+    else if (typeof(window.history.pushState) == 'function') {
+      window.history.pushState(null, "Initial entry", rewrite);
+    }
+    else {
+      window.location.hash = rewrite;
+    }
   }
 })();
