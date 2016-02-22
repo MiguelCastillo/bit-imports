83f9d19ad8a2a2a04f9bf0ad5e661a1d5452ee09
Miguel Castillo <manchagnu@gmail.com>
2016-02-21 23:15:03 -0500

    removed node bootstrap

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-var bitimports;
-var fs         = require('fs');
-var fileReader = require('./src/fileReader');
+var streamProvider = require('promjax');
+var fileReader     = require('./src/fileReader');
 
 // Register method to load file content from storage
-fileReader.register(function read(path) {
-  return new bitimports.Promise(function(resolve, reject) {
-    var filecontent = '';
-    var stream      = fs.createReadStream(path);
-
-    stream.setEncoding('utf8');
-    stream.on('readable', function() {
-        filecontent += stream.read();
-      })
-      .on('end', function() {
-        resolve(filecontent);
-      })
-      .on('error', reject);
-  });
-});
+fileReader.register(streamProvider);
 
 // Export bit imports!
-module.exports = bitimports = require('./src/bit-imports');
+module.exports = require('./src/bit-imports');
