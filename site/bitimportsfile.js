4b08e221b892def626b39ea59563508de611907a
Miguel Castillo <manchagnu@gmail.com>
2016-03-01 12:19:19 -0500

    moved bit-imports loading back to be a script in HTML

--- site/bitimportsfile.js
+++ site/bitimportsfile.js
@@ -1,10 +1,4 @@
-(function (context) {
-  /** modules to load when everything is ready to go */
-  var entries = ["main"];
-
-  /** bit imports module path */
-  var bitimportsPath = "node_modules/bit-imports/dist/bit-imports.js";
-
+var System = (function (global) {
   /** dependency mapping */
   var paths = {
     "loadstyle-bits": "node_modules/loadstyle-bits/dist/index",
@@ -16,8 +10,8 @@
   /** known file extensions that bitimports won't convert to .js */
   var extensions = ["css"];
 
-  loadBitimports().then(function (bitimports) {
-    return bitimports.config({
+  return bitimports
+    .config({
       paths: paths,
       extensions: extensions
     })
@@ -30,12 +24,6 @@
       match: { path: /\.(css)$/ },
       transform: "loadstyle-bits"
     });
-  })
-  .then(function (System) {
-    context.System = System;
-    System.import(entries);
-  });
+})();
 
-  function loadScript(t){var e=document.getElementsByTagName("head")[0]||document.documentElement,a=document.createElement("script");return a.setAttribute("async","true"),a.setAttribute("charset","utf-8"),a.setAttribute("type","text/javascript"),a.setAttribute("src",t),new Promise(function(t){var n=!1;a.onload=a.onreadystatechange=function(){n||this.readyState&&"loaded"!==this.readyState&&"complete"!==this.readyState||(n=!0,a.onload=a.onreadystatechange=null,e&&a.parentNode&&e.removeChild(a),t())},e.appendChild(a)})}
-  function loadBitimports() {return loadScript(bitimportsPath).then(function () {return context.bitimports;});}
-})(this);
+System.import(["main"]);
