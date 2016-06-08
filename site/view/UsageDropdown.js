5bb1b98604e372dd6b91bba979a6c382508585fe
Miguel Castillo <manchagnu@gmail.com>
2016-06-07 22:29:53 -0400

    Adding install instructions

--- site/view/UsageDropdown.js
+++ site/view/UsageDropdown.js
@@ -4,6 +4,7 @@ class UsageDropdown extends Component {
   render() {
     return this.content `
       <ul class="usage-dropdown">
+        <li><a href="install">Install</a></li>
         <li><a href="api">API</a></li>
         <li><a href="cli">CLI</a></li>
         <li><a href="grunt-task">Grunt Task</a></li>
