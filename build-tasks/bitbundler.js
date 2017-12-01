5787885f6ced1e8e7c773cd302b15f04835f32a5
Miguel Castillo <manchagnu@gmail.com>
2017-11-30 23:11:14 -0500

    updated dependencies and fixed breaking changes in the build system

--- build-tasks/bitbundler.js
+++ build-tasks/bitbundler.js
@@ -1,60 +1,46 @@
-var Bitbundler = require("bit-bundler");
-var jsPlugin = require("bit-loader-js");
-var eslintPlugin = require("bit-eslint");
-var builtins = require("bit-loader-builtins");
-var minifyjs = require("bit-bundler-minifyjs");
-var extractsm = require("bit-bundler-extractsm");
-
 module.exports = {
   build: {
-    Bitbundler: Bitbundler,
     files: [{
       src: "browser.js",
       dest: "dist/<%= pkg.name %>.min.js"
     }],
-    loader: {
-      plugins: [
-        eslintPlugin(),
-        jsPlugin(),
-        builtins()
-      ]
-    },
+    loader: [
+      "bit-loader-eslint",
+      "bit-loader-builtins"
+    ],
     bundler: {
       umd: "bitimports",
       plugins: [
-        minifyjs({ banner: buildBannerString() }),
-        extractsm()
+        ["bit-bundler-minifyjs", {
+          output: {
+            beautify: false,
+            preamble: buildBannerString()
+          }
+        }],
+        "bit-bundler-extractsm"
       ]
     }
   },
   dev: {
     watch: true,
-    Bitbundler: Bitbundler,
     files: [{
       src: "test/SpecRunner.js",
       dest: "test/bundle.js"
     }],
-    loader: {
-      plugins: [
-        eslintPlugin(),
-        jsPlugin(),
-        builtins()
-      ]
-    }
+    loader: [
+      "bit-loader-eslint",
+      "bit-loader-builtins"
+    ]
   },
   test: {
-    Bitbundler: Bitbundler,
     files: [{
       src: "test/SpecRunner.js",
       dest: "test/bundle.js"
     }],
-    loader: {
-      plugins: [
-        eslintPlugin(),
-        jsPlugin(),
-        builtins()
-      ]
-    }
+    loader: [
+      "bit-loader-eslint",
+      "bit-loader-builtins"
+    ]
   }
 };
 
